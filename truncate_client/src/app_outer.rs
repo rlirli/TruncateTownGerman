5f714b24f0c51c96ff891ba37c8ef3a6fbc6cb2b
Remi Igbekele <remi.igbekele@am.ai>
2026-03-06 17:14:57 +0100

    ci: Test client code change effect on Github Actions caching

--- truncate_client/src/app_outer.rs
+++ truncate_client/src/app_outer.rs
@@ -150,7 +150,7 @@ impl OuterApplication {
         {
             fonts.font_data.insert(
                 "pixel".into(),
-                egui::FontData::from_static(include_bytes!("../font/m5x7.ttf")),
+                egui::FontData::from_static(include_bytes!("../font/at01.ttf")),
             );
             fonts
                 .families
